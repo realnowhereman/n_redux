7b1b8101840973952cf941aed2fc1e5565446a35
Michail K <learnprogramming15@gmail.com>
2022-10-23 19:49:22 +0300

    finished precomponents version

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const addTodo =() => {
+    if (text.trim().length > 0) {
+      setTodos([
+        ...todos, 
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false,
+        }
+      ])
+      setText('');
+    }
+    
+  }
+
+  const removeTodo = (todoId) => {
+    setTodos(todos.filter(todo => todo.id !==todoId))
+  }
+
+  const toggleTodoComplete = (todoId) => {
+    setTodos.map(
+      todo => {
+        if (todo.id !== todoId) {
+          return todo;
+        }
+        else {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+      }
+    )
+  }
+
+  return (
+    <div className='App'>
+      <label>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
+      </label>
+
+      <ul>
+        {
+          todos.map(todo => <li key={todo.id}>
+            <input type="checkbox" checked={todo.complete} onChange={() => toggleTodoComplete(todo.id)} />
+            <span>{todo.text}</span>
+            <span className='delete' onClick={() => removeTodo(todo.id)}>&times;</span>
+          </li>)
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
